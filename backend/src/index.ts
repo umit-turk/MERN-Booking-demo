0fb5f0a3ae06c9e7d589e0f098162561059d586a
Umit Turk <uyt.turk@gmail.com>
2024-02-12 16:43:15 +0300

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,15 +10,16 @@ import path from "path";
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
