54cd56d83ad371828c0b57fd2ae463d1a85a9f30
umit-turk <uyt_turk@hotmail.com>
2024-02-25 23:38:33 +0300

    I can add a hotel BE and FE

--- frontend/src/forms/ManageHotelForm/DetailsSection.tsx
+++ frontend/src/forms/ManageHotelForm/DetailsSection.tsx
@@ -20,31 +20,71 @@ const DetailsSection = () => {
             </label>
 
             <div className="flex gap-4">
-            <label className="text-gray-700 text-sm font-bold flex-1">
-                City
-                <input
-                    type="text"
-                    className={`border rounded w-full py-1 px-2 font-normal ${errors.city ? "border-red-500" : ""
-                        }`}
-                    {...register("city", { required: "This field is required" })}
-                />
-                {errors.city && (
-                    <span className="text-red-500">{errors.city.message}</span>
-                )}
-            </label>
-            <label className="text-gray-700 text-sm font-bold flex-1">
-                Country
-                <input
-                    type="text"
-                    className={`border rounded w-full py-1 px-2 font-normal ${errors.country ? "border-red-500" : ""
-                        }`}
-                    {...register("country", { required: "This field is required" })}
-                />
-                {errors.country && (
-                    <span className="text-red-500">{errors.country.message}</span>
-                )}
-            </label>
+                <label className="text-gray-700 text-sm font-bold flex-1">
+                    City
+                    <input
+                        type="text"
+                        className={`border rounded w-full py-1 px-2 font-normal ${errors.city ? "border-red-500" : ""
+                            }`}
+                        {...register("city", { required: "This field is required" })}
+                    />
+                    {errors.city && (
+                        <span className="text-red-500">{errors.city.message}</span>
+                    )}
+                </label>
+                <label className="text-gray-700 text-sm font-bold flex-1">
+                    Country
+                    <input
+                        type="text"
+                        className={`border rounded w-full py-1 px-2 font-normal ${errors.country ? "border-red-500" : ""
+                            }`}
+                        {...register("country", { required: "This field is required" })}
+                    />
+                    {errors.country && (
+                        <span className="text-red-500">{errors.country.message}</span>
+                    )}
+                </label>
             </div>
+            <label className="text-gray-700 text-sm font-bold flex-1">
+                Description
+                <textarea
+                    rows={10}
+                    className={`border rounded w-full py-1 px-2 font-normal ${errors.description ? "border-red-500" : ""
+                        }`}
+                    {...register("description", { required: "This field is required" })}
+                />
+                {errors.description && (
+                    <span className="text-red-500">{errors.description.message}</span>
+                )}
+            </label>
+            <label className="text-gray-700 text-sm font-bold max-w-[50%]">
+                Price Per Night
+                <input
+                    type="number"
+                    className={`border rounded w-full py-1 px-2 font-normal ${errors.pricePerNight ? "border-red-500" : ""
+                        }`}
+                    {...register("pricePerNight", { required: "This field is required" })}
+                />
+                {errors.pricePerNight && (
+                    <span className="text-red-500">{errors.pricePerNight.message}</span>
+                )}
+            </label>
+            <label className="text-gray-700 text-sm font-bold max-w-[50%]">
+                Star Rating
+                <select  {...register("starRating", {
+                    required: "This field is required"
+                })}
+                    className="border rounded w-full p-2 text-gray-700 font-normal"
+                >
+                    <option value={""} className="text-sm font-bold" >Select as Rating</option>
+                    {[1, 2, 3, 4, 5].map((num) => (
+                        <option value={num}>{num}</option>
+                    ))}
+                </select>
+                {errors.starRating && (
+                    <span className="text-red-500">{errors.starRating.message}</span>
+                )}
+            </label>
         </div>
     )
 }
--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,8 +1,24 @@
+import { useMutation } from "react-query";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
+import * as apiClient from '../api-client';
 
 const AddHotel = () => {
+    const {showToast} = useAppContext()
+    const {mutate, isLoading} = useMutation(apiClient.addMyHotel, {
+        onSuccess:() => {
+            showToast({message:"Hotel Saved!", type:"SUCCESS"})
+        },
+        onError:() => {
+            showToast({message:"Error saving Hotel",type:"ERROR"})
+        }
+    });
+
+    const handleSave = (HotelFormData:FormData) => {
+        mutate(HotelFormData)
+    }
     return (
-        <ManageHotelForm />
+        <ManageHotelForm onSave={handleSave} isLoading={isLoading} />
     )
 }
 
