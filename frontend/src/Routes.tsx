44cc409e619e2866e9282b0387fbeefe00e86382
umit-turk <uyt_turk@hotmail.com>
2024-05-27 14:46:33 +0300

    search page created

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -6,6 +6,7 @@ import AddHotel from './pages/AddHotel';
 import { useAppContext } from './contexts/AppContext';
 import MyHotels from './pages/MyHotels';
 import EditHotel from './pages/EditHotel';
+import Search from './pages/Search';
 
 function RoutesComponent() {
   const { isLoggedIn } = useAppContext()
@@ -15,6 +16,7 @@ function RoutesComponent() {
         <Route path="/" element={<Layout><p>Home Page</p></Layout>} />
         <Route path="/register" element={<Layout><Register /></Layout>} />
         <Route path="/login" element={<Layout><SignIn /></Layout>} />
+        <Route path='/search' element={<Layout><Search /></Layout>} />
         {isLoggedIn &&
           <>
             <Route path="/add-hotel" element={<Layout><AddHotel /></Layout>} />
---
+++ frontend/src/pages/Search.tsx
@@ -0,0 +1,10 @@
+import { useSearchContext } from '../contexts/SearchContext'
+
+const Search = () => {
+    const search = useSearchContext();
+  return (
+    <div>Search</div>
+  )
+}
+
+export default Search
